70a96ee0119287b8ff6140002eaed7393bbd620b
milen06 <emamaleni55@gmail.com>
2025-06-30 13:03:05 +0800

    update halaman not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,8 @@
+// app/not-found.tsx
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-center px-4">
-      <div>
-        <h1 className="text-3xl font-bold text-pink-400">404 - Halaman Tidak Ditemukan</h1>
-        <p className="mt-2 text-gray-500">Halaman yang kamu cari tidak tersedia.</p>
-      </div>
+    <div className="text-center py-20">
+      <h1 className="text-3xl font-bold">404 - Halaman Tidak Ditemukan</h1>
     </div>
   );
 }
