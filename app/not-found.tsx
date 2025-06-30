e7551741c28c8cf40d95121926f77fa2de5bd637
milen06 <emamaleni55@gmail.com>
2025-06-30 13:11:56 +0800

    Hapus file not-found yang error saat build

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-// app/not-found.tsx
-export default function NotFound() {
-  return (
-    <div className="text-center py-20">
-      <h1 className="text-3xl font-bold">404 - Halaman Tidak Ditemukan</h1>
-    </div>
-  );
-}
